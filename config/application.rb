29b2e2041fdc62f82579d8a6d32b394bcce7c654
seanahrens <seanahrens@gmail.com>
2014-12-23 17:34:04 +0700

    marshall's content changes and application functionality merged and running

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module OpenPprn
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # MARSHALL this line is disabled for Rails < 4.2 (ie. Rails 4.1.7)
+    # config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**/}')]
 
