82803e8c855dbb99fe0e4a6b3ee64e5e81652814
seanahrens <seanahrens@gmail.com>
2014-12-23 23:04:07 +0700

    bugfix to send check in data to oodt

--- app/controllers/concerns/oodt_health_data_controller.rb
+++ app/controllers/concerns/oodt_health_data_controller.rb
@@ -3,7 +3,7 @@ module OODTHealthDataController
 
 
   included do
-      after_action :send_check_in_data_to_oodt_if_complete, :only => [:check_in]
+    after_action :send_check_in_data_to_oodt_if_complete, :only => [:check_in]
   end
 
 
@@ -12,7 +12,7 @@ module OODTHealthDataController
 
   def send_check_in_data_to_oodt_if_complete
     if current_user && @answer_session.completed?
-      current_user.send_check_in_data_to_oodt
+      current_user.send_check_in_data_to_oodt(@answer_session)
     end
   end
 
