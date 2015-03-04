66ffe2424b55b9bc1fb13507a7c32cebadd66b7f
marshallclark <gmarshallclark@gmail.com>
2015-03-04 11:39:32 -0500

    updated to validic sleep calc

--- app/models/user_sleep.rb
+++ app/models/user_sleep.rb
@@ -3,6 +3,8 @@ class UserSleep < ActiveRecord::Base
   validates :validic_obj_id, uniqueness: true
 
   def sleep_hours
-    (total_sleep / 3600).to_f.round(1)
+    sleep = deep + light
+    sleep = total_sleep if sleep.nil? || sleep==0
+    (sleep / 3600).to_f.round(1)
   end
 end
